23a282df4f323d1a26866c094faa567093779ac3
IngGustavo <lx17248@innovaccion.mx>
2022-12-14 22:25:52 -0600

    Add coverage

---
+++ spec/unit/adder.spec.js
@@ -0,0 +1,15 @@
+let sumador = require('../../services/adder');
+
+describe("Test de división", function() {
+    let _numberA;
+    let _numberB;
+
+    it ("Debería devolver la suma entre numberA y numberB", function() {
+        _numberA = 6;
+        _numberB = 7;
+
+        const result = sumador.add(_numberA, _numberB);
+
+        expect(result).toEqual(13);
+    });
+})
